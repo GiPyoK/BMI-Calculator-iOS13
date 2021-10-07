f522ce1dc2873d8432180108372ecdcc590518f2
Gi Pyo Kim <gipyok@gmail.com>
2021-10-06 19:47:05 -0700

    Connected IBOutlets of height and weight labels and updated them with matching sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = round(sender.value * 100) / 100.0
+        heightLabel.text = "\(height)m"
+        
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = Int(sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
 }
 
