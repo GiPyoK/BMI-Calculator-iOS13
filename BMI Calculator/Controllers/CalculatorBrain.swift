46485e50757260e9ce77ec131f020d3288d2652f
Gi Pyo Kim <gipyok@gmail.com>
2021-10-29 12:25:55 -0700

    Separated CalculateViewController and CalculatorBrain logic

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,41 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Gi Pyo Kim on 10/19/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CalculatorBrain {
+    var bmi: BMI?
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight/(height*height)
+        
+        switch bmiValue {
+        case 0..<18.5:
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
+        case 18.5..<24.9:
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        case 24.9... :
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))
+        default:
+            print("Invalid BMI Level")
+        }
+    }
+    
+    func getBMIValue() -> String {
+        return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? ""
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .white
+    }
+}
